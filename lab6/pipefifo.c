588972cb4ae224bafb052b85630cb9a81dbfcaa3
Roman Pankov <extpankov@gmail.com>
2025-11-13 01:46:24 +0300

    fix 6

--- lab6/pipefifo.c
+++ lab6/pipefifo.c
@@ -6,11 +6,12 @@
 #include <time.h>
 
 int main() {
-    int pipefd[2];
+    int p2c[2];
+    int c2p[2];
     pid_t pid;
     char buffer[256];
 
-    if (pipe(pipefd) == -1) {
+    if (pipe(p2c) == -1 || pipe(c2p) == -1) {
         perror("pipe");
         exit(1);
     }
@@ -21,52 +22,69 @@ int main() {
         exit(1);
     }
 
-    if (pid == 0) {  
-        close(pipefd[1]);
+    if (pid == 0) {
+        close(p2c[1]);
+        close(c2p[0]);
 
-        ssize_t bytes;
-        // printf("Дочерний процесс PID=%d\n", getpid());
-
-        while ((bytes = read(pipefd[0], buffer, sizeof(buffer)-1)) > 0) {
-            buffer[bytes] = 0;
-            printf("%s", buffer);
+        char ready = 1;
+        if (write(c2p[1], &ready, 1) == -1) {
+            perror("write ready");
+            exit(1);
         }
 
-        // текущее время ребёнка
+        ssize_t bytes = read(p2c[0], buffer, sizeof(buffer) - 1);
+        if (bytes <= 0) {
+            perror("read");
+            exit(1);
+        }
+        buffer[bytes] = '\0';
+
+        printf("Дочерний процесс PID=%d\n", getpid());
+        printf("%s", buffer);
+
         time_t t = time(NULL);
-        char tstr[32];
         struct tm tm;
+        char tstr[32];
         localtime_r(&t, &tm);
         asctime_r(&tm, tstr);
         tstr[strcspn(tstr, "\n")] = 0;
 
         printf("Время дочернего процесса: %s\n", tstr);
 
-        close(pipefd[0]);
+        close(p2c[0]);
+        close(c2p[1]);
         exit(0);
     } else {
-        // PARENT
-        close(pipefd[0]);
+        close(p2c[0]);
+        close(c2p[1]);
 
-        sleep(5);
+        char ready;
+        if (read(c2p[0], &ready, 1) <= 0) {
+            perror("read ready");
+            exit(1);
+        }
 
         time_t t = time(NULL);
         struct tm tm;
         char tstr[32];
-
         localtime_r(&t, &tm);
         asctime_r(&tm, tstr);
         tstr[strcspn(tstr, "\n")] = 0;
+
+        sleep(5);
 
         char message[256];
         snprintf(message, sizeof(message),
                  "Сообщение от родителя PID=%d\nВремя родителя: %s\n",
                  getpid(), tstr);
 
-        if (write(pipefd[1], message, strlen(message)) == -1)
-            perror("write");
+        if (write(p2c[1], message, strlen(message)) == -1) {
+            perror("write msg");
+            exit(1);
+        }
 
-        close(pipefd[1]);
+        close(p2c[1]);
+        close(c2p[0]);
         wait(NULL);
     }
 
